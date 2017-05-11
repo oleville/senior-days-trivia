957cc48b263faf5ea99ea9abe6119935a7ecc067
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-05-11 14:33:35 -0500

    Add new tables to init script

--- db/init.sql
+++ db/init.sql
@@ -2,5 +2,7 @@
 -- Create tables for Senior Days Quiz Game
 -- Elijah Verdoorn, May 2017
 
-CREATE TABLE teams (id integer primary key, name varchar(100));
+CREATE TABLE teams (id integer primary key, name varchar);
+CREATE TABLE question (id integer primary key, text varchar, answer varchar, catagory_id integer);
+CREATE TABLE category (id integer primary key, text varchar);
 
