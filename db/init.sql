496ed16c531befba1038078f8ba10d21cae72bf7
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-05-18 00:00:25 -0500

    Reformat database init script

--- db/init.sql
+++ db/init.sql
@@ -2,6 +2,23 @@
 -- Create tables for Senior Days Quiz Game
 -- Elijah Verdoorn, May 2017
 
-CREATE TABLE teams (id INTEGER PRIMARY KEY ASC, name VARCHAR NOT NULL);
-CREATE TABLE question (id INTEGER PRIMARY KEY ASC, text VARCHAR, answer VARCHAR, catagory_id INTEGER);
-CREATE TABLE category (id INTEGER PRIMARY KEY ASC, text VARCHAR);
+CREATE TABLE teams (
+	id INTEGER PRIMARY KEY ASC,
+	name VARCHAR NOT NULL
+);
+
+CREATE TABLE question (
+	id INTEGER PRIMARY KEY ASC,
+	text VARCHAR,
+	catagory_id INTEGER
+);
+
+CREATE TABLE category (id INTEGER PRIMARY KEY ASC,
+	text VARCHAR
+);
+
+CREATE TABLE choices (id INNTEGER PRIMARY KEY ASC,
+	FOREIGN KEY(question_id) REFERENCES question(id),
+	text VARCHAR,
+	correct BOOLEAN
+);
