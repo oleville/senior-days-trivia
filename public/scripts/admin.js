7d904d6abb3e7617ebf810b81699cedf97bed24f
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-05-25 11:20:54 -0500

    Add confirmation on reset

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -10,14 +10,16 @@ let nextQuestion = async () => {
 	});
 };
 
-let resetz = async () => {
-	let data = {command: 'resetSession'};
-	fetch('/admin', {
-		method: 'post',
-		body: JSON.stringify(data),
-		headers: new Headers({
-			'Content-Type': 'application/json',
-			Accept: 'application/json',
-		})
-	});
+let reset = async () => {
+	if (confirm('Reset all data and clear this session?')) {
+		let data = {command: 'resetSession'};
+		fetch('/admin', {
+			method: 'post',
+			body: JSON.stringify(data),
+			headers: new Headers({
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			})
+		});
+	}
 };
